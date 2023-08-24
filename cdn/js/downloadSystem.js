40e8c6b00b6b9eef9c81c8fdd1b09662f04e88c7
MegaSquadEfaz <efaz2019@icloud.com>
2023-08-24 13:53:17 -0500

    iframe security

--- cdn/js/downloadSystem.js
+++ cdn/js/downloadSystem.js
@@ -15,36 +15,44 @@ function setIsDownloading(toggle) {
     }
 }
 
-function download() {
-    if (isDownloading == false) {
-        setIsDownloading(true)
-        var url = getDownloadURL()
-        console.log("Loaded URL: " + url)
+function pageInIframe() {
+    return (window.location !== window.parent.location);
+}
 
-        setMessage("Awaiting server..")
-        getIfServerIsActive().then(response => {
-            if (response == true) {
-                setMessage("Starting Download..")
-                if (testingMode == false) {
-                    const object = document.getElementById("downloadbutton")
-                    object.href = url
-                    object.click()
-                    setIsDownloading(false)
-                    setMessage("Download should be started! You'll be redirected soon!")
-    
-                    setTimeout(() => {
-                        object.href = thanksURL
+function download() {
+    if (pageInIframe() == true) {
+        setMessage("Refused to download: This page is inside an iframe.")
+    } else {
+        if (isDownloading == false) {
+            setIsDownloading(true)
+            var url = getDownloadURL()
+            console.log("Loaded URL: " + url)
+
+            setMessage("Awaiting server..")
+            getIfServerIsActive().then(response => {
+                if (response == true) {
+                    setMessage("Starting Download..")
+                    if (testingMode == false) {
+                        const object = document.getElementById("downloadbutton")
+                        object.href = url
                         object.click()
-                    }, "4000");
+                        setIsDownloading(false)
+                        setMessage("Download should be started! You'll be redirected soon!")
+
+                        setTimeout(() => {
+                            object.href = thanksURL
+                            object.click()
+                        }, "4000");
+                    }
+                } else {
+                    setMessage("Server may be down. Try again later!")
+                    setIsDownloading(false)
                 }
-            } else {
+            }).catch(err => {
                 setMessage("Server may be down. Try again later!")
                 setIsDownloading(false)
-            }
-        }).catch(err => {
-            setMessage("Server may be down. Try again later!")
-            setIsDownloading(false)
-        })
+            })
+        }
     }
 }
 
