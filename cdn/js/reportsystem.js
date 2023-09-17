7d967c927488e9dc4835f8892682c3dd4b639b90
MegaSquadEfaz <efaz2019@icloud.com>
2023-09-17 11:17:16 -0500

    x-csrf-token awaiting addon

--- cdn/js/reportsystem.js
+++ cdn/js/reportsystem.js
@@ -105,68 +105,70 @@ function set_mode(mode) {
     })
 }
 
-function on_success_form(args) {}
-function get_xcsrf(args) {
+function on_success_form(args) { }
+async function get_xcsrf(args) {
     return null
 }
 
 function send_response() {
     get_values().then(values => {
-        getModeInfo(selected_mode).then(mode_response => {
-            if (mode_response["success"] == true) {
-                mode_response = mode_response["response"]
-                var new_formated_values = {}
-                var new_api_url = mode_response["api_url"]
-                var listOfKeysProvided = Object.keys(values);
-                var appliedAtSymbol = false
+        get_xcsrf(values).then(x_csrf_token => {
+            getModeInfo(selected_mode).then(mode_response => {
+                if (mode_response["success"] == true) {
+                    mode_response = mode_response["response"]
+                    var new_formated_values = {}
+                    var new_api_url = mode_response["api_url"]
+                    var listOfKeysProvided = Object.keys(values);
+                    var appliedAtSymbol = false
 
-                for (let c = 0; c < listOfKeysProvided.length; c++) {
-                    var key = listOfKeysProvided[c]
-                    var main_val = values[key]
-                    for (let d = 0; d < mode_response["formatted"].length; d++) {
-                        var main_val2 = mode_response["formatted"][d]
-                        if (main_val2["jsonName"] == key) {
-                            if (main_val2["in"] == "Body") {
-                                new_formated_values[key] = main_val
-                            } else if (main_val2["in"] == "URL") {
-                                if (appliedAtSymbol == false) {
-                                    new_api_url = new_api_url + `?${main_val2["jsonName"]}=${main_val}`
-                                    appliedAtSymbol = true
-                                } else {
-                                    new_api_url = new_api_url + `&${main_val2["jsonName"]}=${main_val}`
+                    for (let c = 0; c < listOfKeysProvided.length; c++) {
+                        var key = listOfKeysProvided[c]
+                        var main_val = values[key]
+                        for (let d = 0; d < mode_response["formatted"].length; d++) {
+                            var main_val2 = mode_response["formatted"][d]
+                            if (main_val2["jsonName"] == key) {
+                                if (main_val2["in"] == "Body") {
+                                    new_formated_values[key] = main_val
+                                } else if (main_val2["in"] == "URL") {
+                                    if (appliedAtSymbol == false) {
+                                        new_api_url = new_api_url + `?${main_val2["jsonName"]}=${main_val}`
+                                        appliedAtSymbol = true
+                                    } else {
+                                        new_api_url = new_api_url + `&${main_val2["jsonName"]}=${main_val}`
+                                    }
                                 }
                             }
                         }
                     }
-                }
 
-                var converted_json_string = JSON.stringify(new_formated_values)
-                fetch(new_api_url, {
-                    "headers": {
-                        "accept": "application/json",
-                        "accept-language": "en-US,en;q=0.9",
-                        "content-type": "application/json",
-                        "sec-fetch-dest": "empty",
-                        "sec-fetch-mode": "cors",
-                        "sec-fetch-site": "same-origin",
-                        "x-csrf-token": get_xcsrf(values)
-                    },
-                    "referrerPolicy": "strict-origin-when-cross-origin",
-                    "body": converted_json_string,
-                    "method": "POST",
-                    "mode": "cors",
-                    "credentials": "omit"
-                }).then(res => {
-                    if (res.ok) {
-                        view_success_menu(selected_mode)
-                        on_success_form(values)
-                    } else {
-                        res.json().then(json => {
-                            view_error_menu(json["message"])
-                        })
-                    }
-                })
-            }
+                    var converted_json_string = JSON.stringify(new_formated_values)
+                    fetch(new_api_url, {
+                        "headers": {
+                            "accept": "application/json",
+                            "accept-language": "en-US,en;q=0.9",
+                            "content-type": "application/json",
+                            "sec-fetch-dest": "empty",
+                            "sec-fetch-mode": "cors",
+                            "sec-fetch-site": "same-origin",
+                            "x-csrf-token": x_csrf_token
+                        },
+                        "referrerPolicy": "strict-origin-when-cross-origin",
+                        "body": converted_json_string,
+                        "method": "POST",
+                        "mode": "cors",
+                        "credentials": "omit"
+                    }).then(res => {
+                        if (res.ok) {
+                            view_success_menu(selected_mode)
+                            on_success_form(values)
+                        } else {
+                            res.json().then(json => {
+                                view_error_menu(json["message"])
+                            })
+                        }
+                    })
+                }
+            })
         })
     })
 }
