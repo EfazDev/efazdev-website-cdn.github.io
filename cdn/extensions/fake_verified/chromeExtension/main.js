0185a51a91e324a78813bc39e7389086ec333fae
MegaSquadEfaz <efaz2019@icloud.com>
2024-05-16 17:58:39 -0500

    rah

--- cdn/extensions/fake_verified/chromeExtension/main.js
+++ cdn/extensions/fake_verified/chromeExtension/main.js
@@ -1,59 +1,83 @@
 var latest_code = ""
 var ready = true
+const storage = chrome.storage.sync;
 
-chrome.webNavigation.onDOMContentLoaded.addListener(function (details) {
+chrome.tabs.onUpdated.addListener(function (tabId, details, tab) {
     try {
-        const tabId = details.tabId;
-        if (details.url.includes("https://www.roblox.com")) {
-            const storage = chrome.storage.sync;
-            storage.get(["verified_checkmark_settings"], function (items) {
-                var enabled = true
-                var custom_color = "#0066FF"
-                var group_included = false
-                var allowed_alerts = true
-                var verified_prompt = true
-                var default_prompt = false
-                if (items["verified_checkmark_settings"]) {
-                    if (typeof(items["verified_checkmark_settings"]["enabled"]) == "boolean") { enabled = items["verified_checkmark_settings"]["enabled"] };
-                    if (typeof(items["verified_checkmark_settings"]["color"]) == "string" && /^#[0-9A-F]{6}$/i.test(items["verified_checkmark_settings"]["color"])) { custom_color = items["verified_checkmark_settings"]["color"] };
-                    if (typeof(items["verified_checkmark_settings"]["groupsIncluded"]) == "boolean") { group_included = items["verified_checkmark_settings"]["groupsIncluded"] };
-                    if (typeof(items["verified_checkmark_settings"]["allowedAlerts"]) == "boolean") { allowed_alerts = items["verified_checkmark_settings"]["allowedAlerts"] };
-                    if (typeof(items["verified_checkmark_settings"]["verifiedPrompt"]) == "boolean") { verified_prompt = items["verified_checkmark_settings"]["verifiedPrompt"] };
-                    if (typeof(items["verified_checkmark_settings"]["defaultPrompt"]) == "boolean") { default_prompt = items["verified_checkmark_settings"]["defaultPrompt"] };
-                }
-                if (enabled == true) {
-                    function setVerifiedSettings(data) {
-                        window.verifiedCheckmarkSettings = data
+        if (tab.url) {
+            if (tab.url.startsWith("https://www.roblox.com")) {
+                storage.get(["verified_checkmark_settings"], function (items) {
+                    var enabled = true
+                    var custom_color = "#0066FF"
+                    var group_included = false
+                    var allowed_alerts = true
+                    var verified_prompt = true
+                    var default_prompt = false
+                    if (items["verified_checkmark_settings"]) {
+                        if (typeof (items["verified_checkmark_settings"]["enabled"]) == "boolean") { enabled = items["verified_checkmark_settings"]["enabled"] };
+                        if (typeof (items["verified_checkmark_settings"]["color"]) == "string" && /^#[0-9A-F]{6}$/i.test(items["verified_checkmark_settings"]["color"])) { custom_color = items["verified_checkmark_settings"]["color"] };
+                        if (typeof (items["verified_checkmark_settings"]["groupsIncluded"]) == "boolean") { group_included = items["verified_checkmark_settings"]["groupsIncluded"] };
+                        if (typeof (items["verified_checkmark_settings"]["allowedAlerts"]) == "boolean") { allowed_alerts = items["verified_checkmark_settings"]["allowedAlerts"] };
+                        if (typeof (items["verified_checkmark_settings"]["verifiedPrompt"]) == "boolean") { verified_prompt = items["verified_checkmark_settings"]["verifiedPrompt"] };
+                        if (typeof (items["verified_checkmark_settings"]["defaultPrompt"]) == "boolean") { default_prompt = items["verified_checkmark_settings"]["defaultPrompt"] };
                     }
-                    chrome.scripting.executeScript({
-                        target: { tabId: tabId, allFrames: true },
-                        func: setVerifiedSettings,
-                        args : [ {"enabled": enabled, "color": custom_color, "groupsIncluded": group_included, "allowedAlerts": allowed_alerts, "verifiedPrompt": verified_prompt, "defaultPrompt": default_prompt} ]
-                    });
+                    if (enabled == true) {
+                        function setVerifiedSettings(data) {
+                            window.verifiedCheckmarkSettings = data
+                        }
+                        chrome.scripting.executeScript({
+                            target: { tabId: tabId, allFrames: true },
+                            func: setVerifiedSettings,
+                            args: [{ "enabled": enabled, "color": custom_color, "groupsIncluded": group_included, "allowedAlerts": allowed_alerts, "verifiedPrompt": verified_prompt, "defaultPrompt": default_prompt }]
+                        });
 
-                    if (ready == true) {
-                        fetch("javascript_to_run.js").then(res => {
-                            return res.text()
-                        }).then(code => {
-                            ready = false
+                        if (ready == true) {
+                            fetch("javascript_to_run.js").then(res => {
+                                return res.text()
+                            }).then(code => {
+                                ready = false
+                                chrome.scripting.executeScript({
+                                    target: { tabId: tabId, allFrames: true },
+                                    files: ["javascript_to_run.js"],
+                                });
+                            }).catch(err => {
+                                ready = true
+                                console.error(`Error while getting code from system! ${err.message}`)
+                            })
+                        } else {
                             chrome.scripting.executeScript({
                                 target: { tabId: tabId, allFrames: true },
                                 files: ["javascript_to_run.js"],
                             });
-                        }).catch(err => {
-                            ready = true
-                            console.error(`Error while getting code from system! ${err.message}`)
-                        })
-                    } else {
-                        chrome.scripting.executeScript({
-                            target: { tabId: tabId, allFrames: true },
-                            files: ["javascript_to_run.js"],
-                        });
+                        }
                     }
-                }
-            });
+                });
+            }
         }
     } catch (err) {
         console.log("Failed to add verified badge to this tab.")
     }
+});
+
+chrome.runtime.onInstalled.addListener(() => {
+    const storage = chrome.storage.sync;
+    fetch("settings.json").then(setting_res => {
+        return setting_res.json()
+    }).then(settings => {
+        var name = settings["name"]
+        storage.get([name], async function (items) {
+            if (items[name]) {
+                if (items[name]["thanks"] == true) {
+                    console.log("The extension might have updated!")
+                    return
+                } else {
+                    items[name]["thanks"] = true
+                    chrome.tabs.create({
+                        url: chrome.runtime.getURL("thank_you.html")
+                    })
+                    await storage.set(items);
+                }
+            }
+        });
+    })
 });
--- cdn/extensions/fake_verified/chromeExtension/popup.js
+++ cdn/extensions/fake_verified/chromeExtension/popup.js
@@ -1,65 +1,167 @@
 const storage = chrome.storage.sync
+var system_settings = {}
+
+async function loopThroughArrayAsync(array, callback) {
+    var generated_keys = Object.keys(array);
+    for (a = 0; a < generated_keys.length; a++) {
+        var key = generated_keys[a]
+        var value = array[key]
+        await callback(key, value)
+    }
+}
+
+function loopThroughArray(array, callback) {
+    var generated_keys = Object.keys(array);
+    for (a = 0; a < generated_keys.length; a++) {
+        var key = generated_keys[a]
+        var value = array[key]
+        callback(key, value)
+    }
+}
 
 async function saveData() {
-    const formData = { 
-        "enabled": document.getElementById("enableExtension").checked, 
-        "color": document.getElementById("customColor").value, 
-        "groupsIncluded": document.getElementById("includeGroups").checked, 
-        "allowedAlerts": document.getElementById("allowAlertMessages").checked,
-        "verifiedPrompt": document.getElementById("verifiedPrompt").checked,
-        "defaultPrompt": document.getElementById("defaultPrompt").checked
-    }
-    await storage.set({ "verified_checkmark_settings": formData }, () => {
-        alert("Saved data!")
-    });
-}
-
-async function resetColorInput1() {
-    document.getElementById("customColor").value = "#0066FF";
-}
-
-async function loadChanges(event) {
-    storage.get(["verified_checkmark_settings"], function (items) {
-        var enabled = true
-        var custom_color = "#0066FF"
-        var group_included = false
-        var allowed_alerts = true
-        var verified_prompt = true
-        var default_prompt = false
-        if (items["verified_checkmark_settings"]) {
-            var settings = items["verified_checkmark_settings"];
-            if (typeof(settings["enabled"]) == "boolean") { enabled = settings["enabled"] };
-            if (typeof(settings["color"]) == "string" && /^#[0-9A-F]{6}$/i.test(settings["color"])) { custom_color = settings["color"] };
-            if (typeof(settings["groupsIncluded"]) == "boolean") { group_included = settings["groupsIncluded"] };
-            if (typeof(settings["allowedAlerts"]) == "boolean") { allowed_alerts = settings["allowedAlerts"] };
-            if (typeof(settings["verifiedPrompt"]) == "boolean") { verified_prompt = settings["verifiedPrompt"] };
-            if (typeof(settings["defaultPrompt"]) == "boolean") { default_prompt = settings["defaultPrompt"] };
+    storage.get([system_settings["name"]], async function (items) {
+        if (!(items[system_settings["name"]])) {
+            items[system_settings["name"]] = {}
         }
-        document.getElementById("enableExtension").checked = enabled;
-        document.getElementById("customColor").value = custom_color;
-        document.getElementById("includeGroups").checked = group_included;
-        document.getElementById("allowAlertMessages").checked = allowed_alerts;
-        document.getElementById("verifiedPrompt").checked = verified_prompt;
-        document.getElementById("defaultPrompt").checked = default_prompt;
+        await loopThroughArrayAsync(system_settings["settings"], async (key, val) => {
+            if (val["type"] == "checkbox") {
+                items[system_settings["name"]][key] = document.getElementById(key).checked
+            } else {
+                items[system_settings["name"]][key] = document.getElementById(key).value
+            }
+        });
+        await storage.set(items, () => {
+            alert("Saved data!")
+        });
     });
-    const submitButton = document.getElementById("submitbutton");
-    submitButton.addEventListener("click", saveData);
+}
 
-    const resetColorButton = document.getElementById("resetcolorbutton");
-    resetColorButton.addEventListener("click", resetColorInput1);
+async function loadChanges() {
+    fetch("settings.json").then(setting_res => {
+        return setting_res.json()
+    }).then(settings => {
+        system_settings = settings
+        storage.get([system_settings["name"]], function (items) {
+            if (Object.keys(system_settings["settings"]).length == 1) {
+                document.getElementById("extensionSettings").remove()
+            } else {
+                loopThroughArray(system_settings["settings"], (key, val) => {
+                    if (document.getElementById(key) == null) {
+                        var generated_html_element = `<label for="${key}">${val["text"]}: <input type="${val["type"]}" id="${key}" name="${key}"></label>`
+                        var beforeElement = document.getElementById("reviewDetails")
+                        if (val["reset"] == true) {
+                            generated_html_element = `${generated_html_element} <button id="reset_${key}">Reset!</button>`
+                        }
+                        generated_html_element = `${generated_html_element}<br>`
+                        beforeElement.outerHTML = `${generated_html_element}${document.getElementById("reviewDetails").outerHTML}`
+                    }
+                    var selected = val["default"]
+                    if (items[system_settings["name"]]) {
+                        if (!(typeof(items[system_settings["name"]][key]) == "undefined")) {
+                            selected = items[system_settings["name"]][key]
+                        }
+                    }
+                    if (!(typeof(selected) == "undefined")) {
+                        if (document.getElementById(key)) {
+                            var main_selection = document.getElementById(key)
+                            if (val["type"] == "checkbox") {
+                                main_selection.checked = selected
+                            } else {
+                                main_selection.value = selected
+                            }
+                        }
+                    }
+                    if (val["reset"] == true) {
+                        if (document.getElementById(`reset_${key}`)) {
+                            var button = document.getElementById(`reset_${key}`)
+                            button.addEventListener("click", () => {
+                                var main_selection = document.getElementById(key)
+                                if (val["type"] == "checkbox") {
+                                    main_selection.checked = val["default"]
+                                } else {
+                                    main_selection.value = val["default"]
+                                }
+                            })
+                        }
+                    }
+                })
+            }
+        });
+        const submitButton = document.getElementById("submitbutton");
+        submitButton.addEventListener("click", saveData);
 
-    fetch("manifest.json").then(man_res => {
-        return man_res.json()
-    }).then(man_json => {
-        var extension_name = man_json["name"]
-        var extension_version = man_json["version"]
-        var extension_icon = man_json["icons"]["128"]
+        fetch("manifest.json").then(man_res => {
+            return man_res.json()
+        }).then(man_json => {
+            var extension_name = man_json["name"]
+            var extension_version = man_json["version"]
+            var extension_icon = man_json["icons"]["128"]
 
-        document.getElementById("extens_name").innerHTML = `Extension Name: ${extension_name} ${`<img src="${extension_icon}" height="10" width="10">`}`
-        document.getElementById("extens_vers").innerHTML = `v${extension_version}`
+            document.getElementById("extens_name").innerHTML = `Extension Name: ${extension_name} ${`<img src="${extension_icon}" height="16" width="16" style="vertical-align: middle;">`}`
+            document.getElementById("extens_vers").innerHTML = `v${extension_version}`
+            if (window.location.href.includes("resize=true")) {
+                document.getElementById("css").innerHTML = `
+                p, label, em {
+                    font-size: medium;
+                    text-align: center;
+                    display: block;
+                }
+                h1 {
+                    font-size: xx-large;
+                }
+                li {
+                    display: inline-block;
+                    height: 24px;
+                    width: 24px;
+                    padding: 10px;
+                }
+                .resize-img {
+                    height: 24px;
+                    width: 24px;
+                }
+                ul {
+                    padding-left: 0;
+                    display: flex !important;
+                    justify-content: center;
+                }
+        
+                button {
+                    border: 1px solid white;
+                    border-radius: 8px;
+                    background-color: #00b1ff;
+                    color: rgb(255, 255, 255);
+                    font-family: arialrounded;
+                }
+        
+                input {
+                    border: 1px solid white;
+                    border-collapse: collapse;
+                    text-align: center;
+                    margin-left: auto;
+                    border-radius: 8px;
+                    background-color: #00b1ff;
+                    color: rgb(255, 255, 255);
+                    vertical-align: middle;
+                    justify-content: center;
+                    font-family: arialrounded;
+                    margin-right: auto;
+                }
+        
+                a {
+                    color: #4295f5;
+                }
+                `
+                const style = document.createElement("link")
+                style.id = "resize-to-full-screen";
+                style.rel = "stylesheet";
+                style.type = "text/css";
+                style.media = "all";
+                style.href = "https://cdn.efaz.dev/cdn/other/htmlUI.css"
+                document.head.append(style)
+            }
+        })
     })
 }
 
-window.onload = function () {
-    loadChanges()
-}
+window.onload = loadChanges
