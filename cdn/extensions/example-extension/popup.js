ff16a3ae3f58d168f2341948e6ea14adcc9d87a7
MegaSquadEfaz <efaz2019@icloud.com>
2024-05-20 21:38:44 -0500

    rah

--- cdn/extensions/example-extension/popup.js
+++ cdn/extensions/example-extension/popup.js
@@ -50,10 +50,14 @@ async function loadChanges() {
                     if (document.getElementById(key) == null) {
                         var generated_html_element = `<label for="${key}">${val["text"]}: <input type="${val["type"]}" id="${key}" name="${key}"></label>`
                         var beforeElement = document.getElementById("reviewDetails")
-                        if (val["reset"] == true) {
-                            generated_html_element = `${generated_html_element} <button id="reset_${key}">Reset!</button>`
+                        if (val["hidden"] == true && !(window.location.href.includes("view_hidden=true"))) {
+                            generated_html_element = `<label style="display: none;" for="${key}">${val["text"]}: <input type="${val["type"]}" id="${key}" name="${key}"></label>`
+                        } else {
+                            if (val["reset"] == true) {
+                                generated_html_element = `${generated_html_element} <button id="reset_${key}">Reset!</button>`
+                            }
+                            generated_html_element = `${generated_html_element}<br>`
                         }
-                        generated_html_element = `${generated_html_element}<br>`
                         beforeElement.outerHTML = `${generated_html_element}${document.getElementById("reviewDetails").outerHTML}`
                     }
                     var selected = val["default"]
@@ -100,6 +104,8 @@ async function loadChanges() {
 
             document.getElementById("extens_name").innerHTML = `Extension Name: ${extension_name} ${`<img src="${extension_icon}" height="16" width="16" style="vertical-align: middle;">`}`
             document.getElementById("extens_vers").innerHTML = `v${extension_version}`
+            document.getElementById("window_title").innerText = `${extension_name} Settings`
+
             if (window.location.href.includes("resize=true")) {
                 document.getElementById("css").innerHTML = `
                 p, label, em {
@@ -152,6 +158,9 @@ async function loadChanges() {
                     color: #4295f5;
                 }
                 `
+            }
+
+            if (navigator.onLine && window.location.href.includes("resize=true")) {
                 const style = document.createElement("link")
                 style.id = "resize-to-full-screen";
                 style.rel = "stylesheet";
@@ -159,6 +168,76 @@ async function loadChanges() {
                 style.media = "all";
                 style.href = "https://cdn.efaz.dev/cdn/other/htmlUI.css"
                 document.head.append(style)
+            } else if (navigator.onLine == false && (window.location.href.includes("resize=true"))) {
+                document.getElementById("extens_vers").innerHTML = `${document.getElementById("extens_vers").innerHTML} | Network Offline`
+                document.getElementById("css").innerHTML = `${document.getElementById("css").innerHTML}
+        /* This stylesheet is exported for when your computer has no internet. */
+
+        body {
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            background: linear-gradient(225deg, rgba(255, 75, 0, 1) 0%, rgb(207, 207, 0) 60%, rgb(19, 193, 0) 100%);
+            font-family: arial !important;
+            color: white;
+            overflow: hidden;
+        }
+        p,
+        h1,
+        h2,
+        label {
+            text-align: center;
+        }
+
+        .center {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
+        button {
+            border: 1px solid white;
+            border-collapse: collapse;
+            text-align: center;
+            margin-left: auto;
+            border-radius: 8px;
+            background-color: #00b1ff;
+            color: rgb(255, 255, 255);
+            vertical-align: middle;
+            justify-content: center;
+            font-family: arial;
+            margin-right: auto;
+        }
+        html {
+            width: 100%;
+            height: 100%
+        }
+        div {
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            vertical-align: middle;
+            position: fixed;
+        
+            margin: 0;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            -ms-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+        }
+
+        /* This stylesheet is exported for when your computer has no internet. */
+                `
+            } else if (navigator.onLine == false) {
+                document.getElementById("extens_vers").innerHTML = `${document.getElementById("extens_vers").innerHTML} | Network Offline`
+                document.getElementById("css").innerHTML = `${document.getElementById("css").innerHTML}
+                body {
+                    font-family: arial !important;
+                    color: white;
+                    overflow: hidden;
+                }
+                `
             }
         })
     })
